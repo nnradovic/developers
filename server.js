cf2cafa83acb4a12bc7d8522814ee45518a286b6
Nikola Radovic <nikola.radovic@devtech.local>
2019-08-14 11:11:26 +0200

    connect to mongodb

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const connectDB = require('./config/db')
 
+//Server UP
 const app = express()
 
+//Connect Database
+connectDB()
+
+//Respones route to FRONT 
 app.get('/', (req,res)=>res.send('API Runing'));
 
+//Server runs on port 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, ()=>console.log(`Server started on port ${PORT}`));

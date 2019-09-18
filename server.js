8c1291f940538a1e4e6a2808d4bc5b0b9f037556
Nikola Radovic <nikola.radovic@devtech.local>
2019-09-18 15:01:41 +0200

    frontend setup

--- server.js
+++ server.js
@@ -1,23 +1,32 @@
-const express = require('express');
-const connectDB = require('./config/db')
+const express = require("express");
+const connectDB = require("./config/db");
 
 //Server UP
-const app = express()
+const app = express();
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+}); //Ovo stavi kad  zeza CORS!!!
 
 //Connect Database
-connectDB()
+connectDB();
 
 //Init Middleware to get data from req.body in models
-app.use(express.json({extended:false}));
+app.use(express.json({ extended: false }));
 
 //Define Routes which server use when hit this route and show you your data.
-app.get('/', (req,res)=> res.send('API Running'));
+app.get("/", (req, res) => res.send("API Running"));
 
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/posts', require('./routes/api/posts'));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
-//Server runs on port 
+//Server runs on port
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, ()=>console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

5ffed7a7dcfb9d3348a5bb552a04d3f1343683e1
Nikola Radovic <nikola.radovic@devtech.local>
2019-09-27 13:34:42 +0200

    user authentication

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,11 +1,12 @@
 import React, { Fragment, useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { setAlert } from "../../actions/alert";
+import { register } from "../../actions/auth";
 import PropTypes from "prop-types";
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -18,12 +19,16 @@ const Register = ({ setAlert }) => {
   const onChange = e =>
     setFormData({ ...formData, [e.target.name]: e.target.value }); //pravimo funkciju koja uzima prethiodni fromData i novi data
 
+  //Redirect if logged in
+  if (isAuthenticated) {
+    return <Redirect to="/dashboard" />;
+  }
   const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
       setAlert("Pasword do not match ", "danger");
     } else {
-      console.log("Sucess");
+      register({ name, email, password });
     }
   };
   return (
@@ -87,9 +92,15 @@ const Register = ({ setAlert }) => {
 };
 
 Register.propTypes = {
-  setAlert: PropTypes.func.isRequired
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
 };
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
 export default connect(
-  null,
-  { setAlert }
+  mapStateToProps,
+  { setAlert, register }
 )(Register);

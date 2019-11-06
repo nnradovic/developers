1f5413df7774b2585d3168e6bffc0df169335824
Nikola Radovic <nikola.radovic@devtech.local>
2019-11-06 13:24:02 +0100

    post

--- client/src/App.js
+++ client/src/App.js
@@ -11,8 +11,11 @@ import AddExperience from "./components/profile-forms/AddExperience";
 import AddEducation from "./components/profile-forms/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
+import Posts from "./components/posts/Posts";
+import Post from "./components/post/Post";
 import Dashboard from "./components/dashboard/Dashboard";
 import PrivateRoute from "./components/routing/PrivateRoute";
+
 import setAuthToken from "./utils/setAuthToken";
 import "./App.css";
 
@@ -64,6 +67,8 @@ const App = () => {
                 path="/add-education"
                 component={AddEducation}
               />
+              <PrivateRoute exact path="/posts" component={Posts} />
+              <PrivateRoute exact path="/posts/:id" component={Post} />
             </Switch>
           </section>
         </Fragment>
